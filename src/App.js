bd6615f3aedd9028619cdbbb431ae43b13d70f3b
Brent Muha <800@holbertonschool.com>
2020-05-14 11:45:20 -0700

    Move quiz reducer to separate file

--- src/App.js
+++ src/App.js
@@ -10,49 +10,9 @@ import {
   SET_SHOW_RESULTS,
   RESET_QUIZ,
 } from './reducers/types.js';
+import quizReducer from './reducers/QuizReducer';
 
 import './App.css';
-
-function quizReducer(state, action) {
-  switch(action.type) {
-    case SET_CURRENT_ANSWER:
-      return {
-        ...state,
-        currentAnswer: action.currentAnswer,
-      };
-    case SET_CURRENT_QUESTION:
-      return {
-        ...state,
-        currentQuestion: action.currentQuestion,
-      };
-    case SET_ERROR:
-      return {
-        ...state,
-        error: action.error,
-      };
-    case SET_SHOW_RESULTS:
-      return {
-        ...state,
-        showResults: action.showResults,
-      };
-    case SET_ANSWERS:
-      return {
-        ...state,
-        answers: action.answers,
-      };
-    case RESET_QUIZ:
-      return {
-        ...state,
-        answers: [],
-        currentQuestion: 0,
-        currentAnswer: '',
-        showResults: false,
-        error: ''
-      };
-    default:
-      return state;
-  }
-}
 
 function App() {
   const initialState = {
